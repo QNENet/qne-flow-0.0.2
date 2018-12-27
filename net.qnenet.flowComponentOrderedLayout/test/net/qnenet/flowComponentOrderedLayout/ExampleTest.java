802708cd9a11159742597bd914df5f43e2c7cb51
paulf <paulf@qnenet.com>
2018-12-27 16:05:22 +1100

    works.. yah!!

---
+++ net.qnenet.flowComponentOrderedLayout/test/net/qnenet/flowComponentOrderedLayout/ExampleTest.java
@@ -0,0 +1,14 @@
+package net.qnenet.flowComponentOrderedLayout;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExampleTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
--- net.qnenet.flowOSGiBasicStarter/src/com/example/starter/base/MainView.java
+++ net.qnenet.flowOSGiBasicStarter/src/com/example/starter/base/MainView.java
@@ -4,7 +4,6 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
 
@@ -14,12 +13,14 @@ import com.vaadin.flow.theme.lumo.Lumo;
 @Route("")
 // Need to explicitly declare the Lumo until https://github.com/vaadin/flow/issues/4847 is fixed
 @Theme(Lumo.class)
-@PWA(name = "Project Base for Vaadin Flow", shortName = "Project Base")
+//@PWA(name = "Project Base for Vaadin Flow", shortName = "Project Base")
 public class MainView extends VerticalLayout {
 
-    public MainView() {
-        Button button = new Button("Click me",
-                event -> Notification.show("Clicked!"));
-        add(button);
-    }
+	public MainView() {
+		Button button = new Button("Click me", event -> Notification.show("Clicked!"));
+
+//		Button button = new Button("Click me");
+
+		add(button);
+	}
 }
